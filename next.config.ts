fa9bfc68bc68e238743401528c9065d6a59b19ff
Ubuntu <ubuntu@ip-172-31-9-108.us-east-2.compute.internal>
2026-02-21 16:24:51 +0000

    Configure for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/sof-ia-dashboard',
+  assetPrefix: '/sof-ia-dashboard',
 };
 
 export default nextConfig;
